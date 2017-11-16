135f2a0fceddacabd1fadd1738050daefe8ed44f
vpascal <vpascal2@gmail.com>
2017-11-15 22:11:37 -0500

    update

--- Copier_Summary.py
+++ Copier_Summary.py
@@ -1,40 +1,17 @@
-
-# coding: utf-8
-
-# In[35]:
-
-
 import pandas as pd
 import glob
 
-
-# In[36]:
-
-
 files = glob.glob("*.csv")
 
-
-# In[37]:
-
-
-myheader = ['Copier','User','Cost Center','Total Copies','Color Copies','Cost']
-cols = [6]+list(range(18,23))
-df = pd.concat((pd.read_csv(f,encoding='latin1',header=None,usecols=cols, names=myheader) for f in files))
-df.Cost = df.Cost.replace('[\$,]', '', regex=True).astype(float)
-
-
-# In[38]:
-
+myheader = ['Copier', 'User', 'Cost Center', 'Total Copies', 'Color Copies', 'Cost']
+cols = [6] + list(range(18, 23))
+df = pd.concat((pd.read_csv(f, encoding = 'latin1', header = None, usecols = cols, names = myheader) for f in files))
+df.Cost = df.Cost.replace('[\$,]', '', regex = True).astype(float)
 
 df = df.fillna("N/A")
-grouped = df.groupby(['Copier','User','Cost Center'],as_index=False)['Total Copies','Color Copies','Cost'].sum()
-
-
-# In[39]:
-
+grouped = df.groupby(['Copier', 'User', 'Cost Center'], as_index = False)['Total Copies', 'Color Copies', 'Cost'].sum()
 
 for printer in grouped.Copier.unique():
-      grouped[grouped["Copier"]==printer].to_excel((printer+".xlsx")[10:],index=False) 
-        
-print("Done!")
+  grouped[grouped["Copier"] == printer].to_excel((printer + ".xlsx")[10: ], index = False)
 
+print("Done!")
